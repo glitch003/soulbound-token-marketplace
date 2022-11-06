81080f7143ea40c81db406572130b540c2fc6c68
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-11-06 08:44:23 -0800

    Spelling

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ import LitJsSdk from "lit-js-sdk";
 import OwnedPKPs from "./OwnedPKPs";
 import GetPKP from "./GetPKP";
 import PKPsForSale from "./PKPsForSale";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { getOwnedPKPs, getSigner } from "./lit";
 import { Button, Modal } from "@material-ui/core";
-import {tuncateWalletAddress} from './utils'
-
+import { tuncateWalletAddress } from "./utils";
 
 // for debugging using this stuff in the dev console
 window.ethers = ethers;
 window.LitJsSdk = LitJsSdk;
 
 export default function App() {
-
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
   const [hackyIndex, setHackyIndex] = useState(0);
   const [ownedPkps, setOwnedPkps] = useState(null);
 
@@ -30,51 +28,119 @@ export default function App() {
       const signer = await getSigner();
       const fetchedOwnedPkps = await getOwnedPKPs({ signer });
       setOwnedPkps(fetchedOwnedPkps);
-      setLoading(false)
+      setLoading(false);
     };
     go();
   }, [hackyIndex]);
 
   const rerender = useCallback(() => {
-    setHackyIndex(hackyIndex + 1)
-    setLoading(true)
-  })
-
+    setHackyIndex(hackyIndex + 1);
+    setLoading(true);
+  });
 
   return (
     <div className="App">
-     <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-     <div style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <div style={{minWidth: 300, maxWidth: 350, borderRadius: 20, background:"rgb(210,252,221)", border: "2px solid black", boxShadow: "10px 5px 5px black", padding:"20px", textAlign: "center"}}>
-      <b>What is Soulless?</b>
-      <br></br>    <br></br>
-      Soulless is an interface for creating and selling liquid wallets. A liquid wallet is a transferred key and is made secure by using Lit Protocol, an MPC programmable wallet network. 
-      <br></br>   <br></br>
-      <b>Why is it called Soulless?</b>
-      <br></br>   <br></br>
-      Soulbound tokens are introduced as <b><i>non transferable</i></b> and this application shows that private keys can be securely tradable and as a result these tokens are no longer soul bound, therefore Soulless
-      <div style={{ height: 20 }}></div>
-      <Button onClick={() => setModalOpen(false)} variant="contained" color="primary">Ok</Button>
-      </div>
-      </div>
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              minWidth: 300,
+              maxWidth: 350,
+              borderRadius: 20,
+              background: "rgb(210,252,221)",
+              border: "2px solid black",
+              boxShadow: "10px 5px 5px black",
+              padding: "20px",
+              textAlign: "center",
+            }}
+          >
+            <b>What is Soulless?</b>
+            <br></br> <br></br>
+            Soulless is an interface for creating and selling liquid wallets. A
+            liquid wallet is a transferred key and is made secure by using Lit
+            Protocol, an MPC programmable wallet network.
+            <br></br> <br></br>
+            <b>Why is it called Soulless?</b>
+            <br></br> <br></br>
+            Soulbound tokens are introduced as{" "}
+            <b>
+              <i>non transferable</i>
+            </b>{" "}
+            and this application shows that private keys can be securely
+            tradable and as a result these tokens are no longer soul bound,
+            therefore Soulless
+            <div style={{ height: 20 }}></div>
+            <Button
+              onClick={() => setModalOpen(false)}
+              variant="contained"
+              color="primary"
+            >
+              Ok
+            </Button>
+          </div>
+        </div>
       </Modal>
       <div style={{ height: 50 }}></div>
 
-      <div style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "flex-start"}}>
-      <div style={{height: 55, width: 200, display: "flex", alignItems: "center", justifyContent: "center"}}>
-      <Button onClick={() => setModalOpen(true)} variant="contained" color="primary">About</Button>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+        }}
+      >
+        <div
+          style={{
+            height: 55,
+            width: 200,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            onClick={() => setModalOpen(true)}
+            variant="contained"
+            color="primary"
+          >
+            About
+          </Button>
+        </div>
+        <h1 className="text-anim" style={{ margin: 0 }}>
+          SOULLESS
+        </h1>
+        <div
+          style={{
+            height: 55,
+            width: 200,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            onClick={() => getSigner()}
+            variant="contained"
+            color="primary"
+          >
+            {window.account
+              ? tuncateWalletAddress(window.account)
+              : "Connect Wallet"}
+          </Button>
+        </div>
       </div>
-      <h1 className="text-anim" style={{margin: 0}}>SOULESS</h1>
-      <div style={{height: 55, width: 200, display: "flex", alignItems: "center"}}>
-      <Button onClick={() => getSigner()} variant="contained" color="primary">{window.account ? tuncateWalletAddress(window.account): "Connect Wallet"}</Button>
-      </div>
-      </div>
-
-
 
       <div style={{ height: 50 }}></div>
 
-      <OwnedPKPs rerender={rerender} ownedPkps={ownedPkps} loading={loading}/>
+      <OwnedPKPs rerender={rerender} ownedPkps={ownedPkps} loading={loading} />
 
       <div style={{ height: 50 }}></div>
 
--- src/OwnedPKP.js
+++ src/OwnedPKP.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { useEffect, useState } from "react";
 import {
   getOpenseaUrl,
@@ -8,70 +8,125 @@ import {
 } from "./lit";
 import SendPKP from "./SendPKP";
 import SellPKP from "./SellPKP";
-import { Button, Card, CardContent, CardHeader, Grid, Menu, MenuItem, Tooltip } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@material-ui/core";
 import { copyTextToClipboard, tuncateWalletAddress } from "./utils";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-export default function OwnedPKP({pkp, rerender }) {
+export default function OwnedPKP({ pkp, rerender }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const menuOpen = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const menuOpen = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-
-    return  <Grid item  xs={4} sm={4} md={4} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-    <div style={{minWidth: 300, maxWidth: 350, borderRadius: 20, background:"rgb(210,252,221)", border: "2px solid black", boxShadow: "10px 5px 5px black", padding:"20px"}}  key={pkp.tokenId}>
-      <CardHeader title={
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-        <Tooltip title="Click to copy"><div onClick={() => {
-            copyTextToClipboard(pkp.ethAddress)
-            toast("Copied address to clipboard")
-            }} 
-            style={{backgroundColor: "rgb(220, 49, 88)", borderRadius: 20, paddingLeft: 10, paddingRight: 10, paddingTop: 3, paddingBottom: 3}}>{tuncateWalletAddress(pkp.ethAddress)}</div></Tooltip>
-        <Button 
-        variant="contained" 
-        color="primary"
-        id="basic-button"
-        aria-controls={menuOpen ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={menuOpen ? 'true' : undefined}
-        onClick={handleClick}>
-            Menu
-        </Button>
-        <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={menuOpen}
-            onClose={handleClose}
-            MenuListProps={{
-            'aria-labelledby': 'basic-button',
-            }}
-          >
-            <SendPKP rerender={rerender} tokenId={pkp.tokenId}>Send</SendPKP>
-            <SellPKP tokenId={pkp.tokenId} />
-            <MenuItem onClick={() => window.open(pkp.openseaUrl)}>View</MenuItem>
-          </Menu>
-        </div>}>
-        </CardHeader> 
-      <div>
-        <h4>NFTs/SBTs owned by wallet</h4>
-        {pkp.nftsThisWalletOwns.ownedNfts.length ? pkp.nftsThisWalletOwns.ownedNfts.map((n) => (
-          <a
-            style={{wordWrap: "break-word"}}
-            href={getOpenseaUrlForAnyNft({
-              contractAddress: n.contract.address,
-              tokenId: n.id.tokenId,
-            })}
-            target="_blank"
-          >
-            {n.metadata.name}
-          </a>
-        )) : "None"}
+  return (
+    <Grid
+      item
+      xs={4}
+      sm={4}
+      md={4}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          minWidth: 300,
+          maxWidth: 350,
+          borderRadius: 20,
+          background: "rgb(210,252,221)",
+          border: "2px solid black",
+          boxShadow: "10px 5px 5px black",
+          padding: "20px",
+        }}
+        key={pkp.tokenId}
+      >
+        <CardHeader
+          title={
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <Tooltip title="Click to copy">
+                <div
+                  onClick={() => {
+                    copyTextToClipboard(pkp.ethAddress);
+                    toast("Copied address to clipboard");
+                  }}
+                  style={{
+                    backgroundColor: "rgb(220, 49, 88)",
+                    borderRadius: 20,
+                    paddingLeft: 10,
+                    paddingRight: 10,
+                    paddingTop: 3,
+                    paddingBottom: 3,
+                  }}
+                >
+                  {tuncateWalletAddress(pkp.ethAddress)}
+                </div>
+              </Tooltip>
+              <Button
+                variant="contained"
+                color="primary"
+                id="basic-button"
+                aria-controls={menuOpen ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={menuOpen ? "true" : undefined}
+                onClick={handleClick}
+              >
+                Menu
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={menuOpen}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <SendPKP rerender={rerender} tokenId={pkp.tokenId}>
+                  Send
+                </SendPKP>
+                <SellPKP tokenId={pkp.tokenId} />
+                <MenuItem onClick={() => window.open(pkp.openseaUrl)}>
+                  View
+                </MenuItem>
+              </Menu>
+            </div>
+          }
+        ></CardHeader>
+        <div>
+          <h4>NFTs/SBTs owned by wallet</h4>
+          {pkp.nftsThisWalletOwns.ownedNfts.length
+            ? pkp.nftsThisWalletOwns.ownedNfts.map((n) => (
+                <div>
+                  <a
+                    style={{ wordWrap: "break-word" }}
+                    href={getOpenseaUrlForAnyNft({
+                      contractAddress: n.contract.address,
+                      tokenId: n.id.tokenId,
+                    })}
+                    target="_blank"
+                  >
+                    {n.metadata.name}
+                  </a>
+                </div>
+              ))
+            : "None"}
+        </div>
       </div>
-    </div>
-  </Grid>
+    </Grid>
+  );
 }
